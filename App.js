4523fdced370f84ac6fcb5931a93b5be33ee8fde
Atopiano <121912367+Atopiano@users.noreply.github.com>
2023-05-22 11:43:35 +0900

    안드러이드, 아이폰 뒤로가기 기능 및 아이폰은 버튼 추가

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
-import * as React from 'react';
-import { WebView } from 'react-native-webview';
+import React from 'react';
+import { StyleSheet, View,Platform } from 'react-native';
+import IosBrowser from './IosBrowser';
+import AndBrowser from './AndBrowser';
 
 
-export default function App() {
-  return (
-    <WebView
-      source={{ uri: 'https://hobt.co.kr' }}
-    />
-  );
+
+
+export default class App extends React.Component {
+  
+  render() {
+    const iOSPlatform = <IosBrowser/>;
+    const androidPlatform = <AndBrowser/>;
+
+    return (
+      <View style={styles.root}>
+        <View style={styles.browser}>
+          {Platform.OS === 'ios'? iOSPlatform : androidPlatform}
+        </View>
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  browser: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  root: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    display: 'flex'
+  },
+});
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# HoBTapp_Webview
+expo 웹뷰로 만든 HoBT 웹앱입니다.
